574f434a2323bdbf549a36922b2c60524505de1d
Suro Zakaryan <suren.zakaryan177@gmail.com>
2020-05-30 11:45:13 +0400

    My Gorilla Project

---
+++ js/script.js
@@ -0,0 +1,34 @@
+new WOW().init();
+
+// document.getElementById("test").style.width="4%";
+// document.getElementById("test").style.right="14%";
+
+//   	window.onload = function() {
+//      var pos = 0; 
+//     //our box element
+//     var box = document.getElementById('test');
+//     var t = setInterval(move, 140);
+  
+//     function move() {
+//       if(pos >= 1) {
+//           clearInterval(t);
+//         }
+//         else {
+//           pos += 1;
+//           box.style.right = pos+'px';
+//         }
+//        }
+//       };
+
+//       window.onscroll = function() {
+//         var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+//         document.getElementById('showScroll').innerHTML = scrolled + 'px';
+//       }
+
+//       window.onscroll = function() {
+//         var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+//         document.getElementById('showScroll').innerHTML = scrolled + 'px';
+//       }
+      
+
+//  
